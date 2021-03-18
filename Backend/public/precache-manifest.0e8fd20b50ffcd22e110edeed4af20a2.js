1ebdaa15659f02e3d7e362dd39bef558ebf4e3e5
Oscar <oscarmayor0211@gmail.com>
2021-03-18 17:05:27 -0500

    first commit

---
+++ Backend/public/precache-manifest.0e8fd20b50ffcd22e110edeed4af20a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835d4c2fee83f2070c0fadc234225824",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "4b25aa7b7755c1488db7",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/js/2.bd64b146.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.bd64b146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b25aa7b7755c1488db7",
+    "url": "/static/js/main.16f935e1.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
---
+++ Backend/public/precache-manifest.8741e5e7ba590a8e89d203e231ae880c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbba9c9b95018e3765d4172f3c101ef6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "1b5dc27505de82734c3c",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/js/2.bd64b146.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.bd64b146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b5dc27505de82734c3c",
+    "url": "/static/js/main.84bba68d.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
